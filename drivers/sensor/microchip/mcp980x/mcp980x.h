c1383298fcc9dafdbae0b6123b7eec88aed033bc
Matthias Alleman <matthias.alleman@basalte.be>
2024-10-02 14:53:12 +0200

    drivers: sensor: microchip: Add mcp980x sensor
    This driver adds support for the MCP9800/9801/9802/9803 digital
    temperature sensors.
    
    Signed-off-by: Matthias Alleman <matthias.alleman@basalte.be>

---
+++ drivers/sensor/microchip/mcp980x/mcp980x.h
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2024 Basalte bv
+ * Copyright (c) 2019 Peter Bigot Consulting, LLC
+ * Copyright (c) 2016 Intel Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef ZEPHYR_DRIVERS_SENSOR_MCP980X_MCP980X_H_
+#define ZEPHYR_DRIVERS_SENSOR_MCP980X_MCP980X_H_
+
+#include <errno.h>
+
+#include <zephyr/types.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/sensor.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/drivers/gpio.h>
+
+#define MCP980X_REG_TEMP_AMB 0x00
+#define MCP980X_REG_CONFIG   0x01
+#define MCP980X_REG_HYST     0x02
+#define MCP980X_REG_LIMIT    0x03
+
+/* 8 bits control configuration and state.
+ *
+ * * Bit 0 controls shutdown mode
+ * * Bit 1 controls interrupt/comparator mode
+ * * Bit 2 controls alert polarity
+ * * Bit 3-4 fault queue bits
+ * * Bit 5-6 ADC resolution bits
+ * * Bit 7 one-shot enable bit
+ */
+#define MCP980X_CFG_SHUTDOWN       BIT(0)
+#define MCP980X_CFG_INT_COMP_MODE  BIT(1)
+#define MCP980X_CFG_ALERT_POL      BIT(2)
+#define MCP980X_CFG_FAULT_QUEUE    (BIT(3) | BIT(4))
+#define MCP980X_CFG_ADC_RESOLUTION (BIT(5) | BIT(6))
+#define MCP980X_CFG_ONE_SHOT       BIT(7)
+
+/* 12 bits are used for temperature and state encoding:
+ * * Bits 4..15 encode the temperature in a 2s complement signed value
+ *   in Celsius with 1/16 Cel resolution
+ * * Bit 16 is set to indicate a negative temperature
+ */
+#define MCP980X_TEMP_SCALE_CEL 256 /* signed */
+#define MCP980X_TEMP_SIGN_BIT  BIT(15)
+#define MCP980X_TEMP_ABS_MASK  ((uint16_t)(MCP980X_TEMP_SIGN_BIT - 15U))
+
+struct mcp980x_data {
+	uint16_t reg_val;
+
+#ifdef CONFIG_MCP980X_TRIGGER
+	struct gpio_callback alert_cb;
+
+	const struct device *dev;
+
+	const struct sensor_trigger *trig;
+	sensor_trigger_handler_t trigger_handler;
+#endif
+
+#ifdef CONFIG_MCP980X_TRIGGER_OWN_THREAD
+	struct k_sem sem;
+#endif
+
+#ifdef CONFIG_MCP980X_TRIGGER_GLOBAL_THREAD
+	struct k_work work;
+#endif
+};
+
+struct mcp980x_config {
+	struct i2c_dt_spec i2c;
+	uint8_t resolution;
+#ifdef CONFIG_MCP980X_TRIGGER
+	struct gpio_dt_spec int_gpio;
+#endif /* CONFIG_MCP980X_TRIGGER */
+};
+
+int mcp980x_reg_read(const struct device *dev, uint8_t reg, uint16_t *val, uint8_t len);
+int mcp980x_reg_write(const struct device *dev, uint8_t reg, uint16_t val, uint8_t len);
+
+#ifdef CONFIG_MCP980X_TRIGGER
+int mcp980x_attr_set(const struct device *dev, enum sensor_channel chan, enum sensor_attribute attr,
+		     const struct sensor_value *val);
+int mcp980x_trigger_set(const struct device *dev, const struct sensor_trigger *trig,
+			sensor_trigger_handler_t handler);
+int mcp980x_setup_interrupt(const struct device *dev);
+#endif /* CONFIG_MCP980X_TRIGGER */
+
+/* Encode a signed temperature in scaled Celsius to the format used in
+ * register values.
+ */
+static inline uint16_t mcp980x_temp_reg_from_signed(int temp)
+{
+	/* Get the 12-bit 2s complement value */
+	uint16_t rv = temp & MCP980X_TEMP_ABS_MASK;
+
+	if (temp < 0) {
+		rv |= MCP980X_TEMP_SIGN_BIT;
+	}
+	return rv;
+}
+
+/* Decode a register temperature value to a signed temperature in
+ * scaled Celsius.
+ */
+static inline int mcp980x_temp_signed_from_reg(uint16_t reg)
+{
+	int rv = reg & MCP980X_TEMP_ABS_MASK;
+
+	if (reg & MCP980X_TEMP_SIGN_BIT) {
+		/* Convert 12-bit 2s complement to signed negative
+		 * value.
+		 */
+		rv = -(1U + (rv ^ MCP980X_TEMP_ABS_MASK));
+	}
+	return rv;
+}
+
+#endif /* ZEPHYR_DRIVERS_SENSOR_MCP980X_MCP980X_H_ */
---
+++ drivers/sensor/microchip/mcp980x/mcp980x_trigger.c
@@ -0,0 +1,199 @@
+/*
+ * Copyright (c) 2024 Basalte bv
+ * Copyright (c) 2019 Peter Bigot Consulting, LLC
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <errno.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/sys/byteorder.h>
+#include <zephyr/sys/__assert.h>
+#include <zephyr/logging/log.h>
+#include "mcp980x.h"
+
+LOG_MODULE_DECLARE(MCP980X, CONFIG_SENSOR_LOG_LEVEL);
+
+int mcp980x_attr_set(const struct device *dev, enum sensor_channel chan, enum sensor_attribute attr,
+		     const struct sensor_value *val)
+{
+	const struct mcp980x_config *cfg = dev->config;
+	uint8_t reg_addr;
+	int temp;
+	int ret;
+
+	__ASSERT_NO_MSG(chan == SENSOR_CHAN_AMBIENT_TEMP);
+
+	if (!gpio_is_ready_dt(&cfg->int_gpio)) {
+		LOG_ERR("Gpio is not ready");
+		return -ENOTSUP;
+	}
+
+	switch (attr) {
+	case SENSOR_ATTR_UPPER_THRESH:
+		reg_addr = MCP980X_REG_LIMIT;
+		break;
+	case SENSOR_ATTR_HYSTERESIS:
+		reg_addr = MCP980X_REG_HYST;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	/* Convert temperature to a signed scaled value, then write
+	 * the 12-bit 2s complement-plus-sign-bit register value.
+	 */
+	temp = val->val1 * MCP980X_TEMP_SCALE_CEL;
+	temp += (MCP980X_TEMP_SCALE_CEL * val->val2) / 1000000;
+
+	ret = mcp980x_reg_write(dev, reg_addr, mcp980x_temp_reg_from_signed(temp),
+				sizeof(uint16_t));
+	if (ret < 0) {
+		LOG_ERR("Failed to write reg (%d)", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+static inline void setup_int(const struct device *dev, bool enable)
+{
+	const struct mcp980x_config *cfg = dev->config;
+	unsigned int flags = enable ? GPIO_INT_EDGE_TO_ACTIVE : GPIO_INT_DISABLE;
+
+	gpio_pin_interrupt_configure_dt(&cfg->int_gpio, flags);
+}
+
+static void handle_int(const struct device *dev)
+{
+	struct mcp980x_data *data = dev->data;
+
+	setup_int(dev, false);
+
+#if defined(CONFIG_MCP980X_TRIGGER_OWN_THREAD)
+	k_sem_give(&data->sem);
+#elif defined(CONFIG_MCP980X_TRIGGER_GLOBAL_THREAD)
+	k_work_submit(&data->work);
+#endif
+}
+
+static void process_int(const struct device *dev)
+{
+	struct mcp980x_data *data = dev->data;
+
+	if (data->trigger_handler) {
+		data->trigger_handler(dev, data->trig);
+	}
+
+	if (data->trigger_handler) {
+		setup_int(dev, true);
+	}
+}
+
+int mcp980x_trigger_set(const struct device *dev, const struct sensor_trigger *trig,
+			sensor_trigger_handler_t handler)
+{
+	struct mcp980x_data *data = dev->data;
+	const struct mcp980x_config *cfg = dev->config;
+	int rv = 0;
+
+	if (!gpio_is_ready_dt(&cfg->int_gpio)) {
+		LOG_ERR("Gpio is not ready");
+		return -ENOTSUP;
+	}
+
+	setup_int(dev, false);
+
+	data->trig = trig;
+	data->trigger_handler = handler;
+
+	if (handler != NULL) {
+		setup_int(dev, true);
+
+		rv = gpio_pin_get_dt(&cfg->int_gpio);
+		if (rv > 0) {
+			handle_int(dev);
+			rv = 0;
+		}
+	}
+
+	return rv;
+}
+
+static void alert_cb(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
+{
+	struct mcp980x_data *data = CONTAINER_OF(cb, struct mcp980x_data, alert_cb);
+
+	ARG_UNUSED(pins);
+
+	handle_int(data->dev);
+}
+
+#ifdef CONFIG_MCP980X_TRIGGER_OWN_THREAD
+
+static void mcp980x_thread_main(void *p1, void *p2, void *p3)
+{
+	ARG_UNUSED(p2);
+	ARG_UNUSED(p3);
+
+	struct mcp980x_data *data = p1;
+
+	while (true) {
+		k_sem_take(&data->sem, K_FOREVER);
+		process_int(data->dev);
+	}
+}
+
+static K_KERNEL_STACK_DEFINE(mcp980x_thread_stack, CONFIG_MCP980X_THREAD_STACK_SIZE);
+static struct k_thread mcp980x_thread;
+#else /* CONFIG_MCP980X_TRIGGER_GLOBAL_THREAD */
+
+static void mcp980x_gpio_thread_cb(struct k_work *work)
+{
+	struct mcp980x_data *data = CONTAINER_OF(work, struct mcp980x_data, work);
+
+	process_int(data->dev);
+}
+
+#endif /* CONFIG_MCP980X_TRIGGER_GLOBAL_THREAD */
+
+int mcp980x_setup_interrupt(const struct device *dev)
+{
+	struct mcp980x_data *data = dev->data;
+	const struct mcp980x_config *cfg = dev->config;
+	int ret;
+
+	data->dev = dev;
+
+#ifdef CONFIG_MCP980X_TRIGGER_OWN_THREAD
+	k_sem_init(&data->sem, 0, K_SEM_MAX_LIMIT);
+
+	k_thread_create(&mcp980x_thread, mcp980x_thread_stack, CONFIG_MCP980X_THREAD_STACK_SIZE,
+			mcp980x_thread_main, data, NULL, NULL,
+			K_PRIO_COOP(CONFIG_MCP980X_THREAD_PRIORITY), 0, K_NO_WAIT);
+#else  /* CONFIG_MCP980X_TRIGGER_GLOBAL_THREAD */
+	data->work.handler = mcp980x_gpio_thread_cb;
+#endif /* trigger type */
+
+	if (!gpio_is_ready_dt(&cfg->int_gpio)) {
+		LOG_ERR("GPIO device not ready");
+		return -ENODEV;
+	}
+
+	ret = gpio_pin_configure_dt(&cfg->int_gpio, GPIO_INPUT);
+	if (ret < 0) {
+		LOG_ERR("Failed to configure pin (%d)", ret);
+		return ret;
+	}
+
+	gpio_init_callback(&data->alert_cb, alert_cb, BIT(cfg->int_gpio.pin));
+
+	ret = gpio_add_callback(cfg->int_gpio.port, &data->alert_cb);
+	if (ret < 0) {
+		LOG_ERR("Failed to add callback (%d)", ret);
+		return ret;
+	}
+
+	return ret;
+}
